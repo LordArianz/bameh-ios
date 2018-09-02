e16cd7b94a4b3704c3094ba669c527b59a375b93
LordArianz <morteza758@gmail.com>
2018-09-02 22:27:29 +0430

    Initial Commit

---
+++ Bameh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bameh
+//
+//  Created by S.Morteza Ezzabady on 9/2/18.
+//  Copyright © 2018 S.Morteza Ezzabady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
